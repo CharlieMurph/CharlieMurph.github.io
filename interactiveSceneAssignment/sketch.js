0915344649cf067b4d44f14cd4988d6d70ae60b3
Murphy3200860 <3200860@sps-student.sk.ca>
2018-09-20 13:29:14 -0600

    added code

--- interactiveSceneAssignment/sketch.js
+++ interactiveSceneAssignment/sketch.js
@@ -5,12 +5,11 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+let mysong;
 
-function draw() {
- background(85)
+function preload() {
+  soundFormats("mp3");
+  mysong = loadSound("assets/allstar.mp3");
 }
 
 function keyTyped() {
@@ -20,8 +19,21 @@ function keyTyped() {
   else if (key === "b") {
     background(0);
   }
+  else if (key === "p") {
+    mysong.Volume(50);
+    mysong.play();
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(55);
+}
+
+function draw() {
+ keytyped();
 }
