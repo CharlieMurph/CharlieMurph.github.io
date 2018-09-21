a2a8d699402c7b0c2835a3a90787f6e60784d1b4
Murphy3200860 <3200860@sps-student.sk.ca>
2018-09-21 13:17:01 -0600

    changed/added code

--- interactiveSceneAssignment/sketch.js
+++ interactiveSceneAssignment/sketch.js
@@ -6,22 +6,43 @@
 // - describe what you did to take this project "above and beyond"
 
 let mysong;
+let smash;
+let x, y;
+let dx, dy;
+let sheet;
 
 function preload() {
-  soundFormats("mp3");
   mysong = loadSound("assets/allstar.mp3");
+  mysong.setVolume(100);
+  smash = loadImage("assets/smash.png");
 }
 
 function keyTyped() {
   if (key === "w") {
-    background(255);
+    sheet == (255);
   }
   else if (key === "b") {
-    background(0);
+    sheet = (0);
   }
   else if (key === "p") {
-    mysong.Volume(50);
-    mysong.play();
+    if (mysong.isPlaying()) {
+      mysong.pause();
+    }
+    else {
+      mysong.play();
+      mysong.loop();
+    }
+  }
+}
+
+function movesmash() {
+  x += dx
+  y += dy
+  if (x + smash.width > width || x < 0) {
+    dx = dx * -1
+  }
+  else if (y + smash.height > height || y < 0) {
+    dy = dy * -1
   }
 }
 
@@ -29,11 +50,23 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+function displaysmash() {
+  // background(sheet);
+  image(smash, x, y);
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(55);
+  x = width / 2 - smash.width / 2;
+  y = height / 2 - smash.height / 2;
+  dx = random(2, 8)
+  dy = random(2, 8)
 }
 
 function draw() {
- keytyped();
+ keyTyped();
+ displaysmash();
+ movesmash();
+
 }
