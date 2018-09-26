ed5ce7b6590827bb708dc3dfe20aff3c182f5e69
Murphy3200860 <3200860@sps-student.sk.ca>
2018-09-26 13:57:53 -0600

    New Assignment base file

---
+++ stateVariableAssignment/sketch.js
@@ -0,0 +1,27 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let state;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  state = 0;
+}
+
+function draw() {
+
+}
+
+function determineState() {
+  if (state === 1) {
+    displayMainMenu();
+  }
+}
+
+function displayMainMenu() {
+  background(0);
+}
