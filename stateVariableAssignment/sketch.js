c63f6cda4a739f6a7c4e93857cd0f46427b8744e
Murphy3200860 <3200860@sps-student.sk.ca>
2018-09-27 14:07:16 -0600

    added code

--- stateVariableAssignment/sketch.js
+++ stateVariableAssignment/sketch.js
@@ -6,6 +6,12 @@
 // - describe what you did to take this project "above and beyond"
 
 let state;
+const BUTTON_WIDTH = 200;
+const BUTTON_HEIGHT = 100;
+let diameter, sliceWidth;
+let x, y;
+let mouseReticleColour = ["white", "red", "white", "red"];
+let i;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,15 +19,47 @@ function setup() {
 }
 
 function draw() {
-
+  determineState();
+  mouseReticle();
 }
 
 function determineState() {
-  if (state === 1) {
+  if (state === 0) {
     displayMainMenu();
   }
 }
 
 function displayMainMenu() {
-  background(0);
+  background(100);
+  displayButton1();
+  displayButton2();
+}
+
+function displayButton1() {
+  fill(100, 200, 0);
+  strokeWeight(5);
+  stroke(255, 0, 0);
+  rect(windowWidth / 2 - BUTTON_WIDTH/ 2, windowHeight / 2 + BUTTON_HEIGHT, BUTTON_WIDTH,BUTTON_HEIGHT);
+  fill (0);
+  strokeWeight(10);
+  text("Game 1",windowWidth / 2 - BUTTON_WIDTH / 2, windowHeight / 2 - BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT);
+}
+
+function displayButton2() {
+  fill(100, 200, 0);
+  strokeWeight(5);
+  stroke(255, 0, 0);
+  rect(windowWidth / 2 - BUTTON_WIDTH/ 2, windowHeight / 2 - BUTTON_HEIGHT, BUTTON_WIDTH,BUTTON_HEIGHT);
+}
+
+function mouseReticle() {
+  diameter = 40;
+  sliceWidth = diameter/ 4;
+  for (let i = 0; i < 4; i++) {
+    fill (mouseReticleColour[i]);
+    stroke (0);
+    strokeWeight(2);
+    ellipse(mouseX,mouseY,diameter, diameter);
+    diameter -= sliceWidth;
+  }
 }
