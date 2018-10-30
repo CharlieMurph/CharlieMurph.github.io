a55ab3dead64330968a55240cee65404f234dceb
Murphy3200860 <3200860@sps-student.sk.ca>
2018-10-30 14:11:10 -0600

    updates

--- loadingFile/sketch.js
+++ loadingFile/sketch.js
@@ -1,16 +1,23 @@
 // Project Title
 // Your Name
 // Date
-let rows = 10;
-let cols = 10;
+let rows;
+let cols;
 let grid;
 let cellSize;
+
+function preload() {
+  grid = loadStrings("assets/grid1.txt");
+}
 
 function setup() {
   createCanvas(600, 600);
   background(220);
+  rows = grid[0].length;
+  cols = grid[0].length;
   cellSize = width / cols;
-  grid = createRandom2DArray(cols, rows);
+  //grid = createRandom2DArray(cols, rows);
+  cleanUpTheGrid();
 }
 
 function draw() {
@@ -18,10 +25,16 @@ function draw() {
   displayGrid();
 }
 
+function cleanUpTheGrid() {
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = grid[i].split(""); //turns it into a 2D array
+  }
+}
+
 function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      if (grid[y][x] === 0) {
+      if (grid[y][x] === "0") {
         fill(0);
       }
       else {
@@ -38,10 +51,10 @@ function createRandom2DArray(cols, rows) {
     randomGrid.push([]);
     for (let x = 0; x < cols; x++) {
       if (random(100) < 50) {
-        randomGrid[y].push(0);
+        randomGrid[y].push("0");
       }
       else {
-        randomGrid[y].push(1);
+        randomGrid[y].push("1");
       }
     }
   }
@@ -53,10 +66,10 @@ function mousePressed(){
   let x = floor(mouseX / cellSize);
   let y = floor(mouseY / cellSize);
 
-  if (grid[y][x] === 1) {
-    grid[y][x] = 0;
+  if (grid[y][x] === "1") {
+    grid[y][x] = "0";
   }
-  else if (grid[y][x] === 0) {
-    grid[y][x] = 1;
+  else if (grid[y][x] === "0") {
+    grid[y][x] = "1";
   }
 }
